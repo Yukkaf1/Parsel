b1d545d327447b20e109b9935a955f829afe0c31
Trunova Yulia <trunovaf11@gmail.com>
2022-11-26 11:37:35 +0100

    express

---
+++ app.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+const uuidV4 = require('uuid.v4')
+
+
+// const passwordSchema = Joi.string()
+//         .alphanum()
+//         .min(3)
+//         .max(5);
+
+// console.log(passwordSchema.validate("jg123j"));
+
+// console.log("Hallo");
+
+console.log(uuidV4());
+
+
