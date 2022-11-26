2f2a7f7cb28c093479e60d78faec15e1d9e99b29
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2022-11-26 09:53:46 +0100

    Initial commit

---
+++ src/api-service.js
@@ -0,0 +1,4 @@
+export const v = 5;
+export const a = 5;
+export const b = 5;
+export const c = 5;
---
+++ src/app.js
@@ -0,0 +1,5 @@
+console.log('Hello world!');
+
+import * as value from './api-service.js';
+
+console.log(value.v);
